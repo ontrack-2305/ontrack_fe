39c3114d3714ed3ed824a9d2e30e4833ae2159ff
Dani Wilson <danirae.powell@gmail.com>
2023-09-12 17:32:58 -0600

    add route to sessions omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "welcome#index"
+
+  get "/auth/:provider/callback" => "sessions#omniauth"
 end
