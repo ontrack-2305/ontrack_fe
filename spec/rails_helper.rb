c6d9919f082ab6c98842f6c51b5835236e8d85e9
Artemy Gibson <artemygibson@gmail.com>
2023-09-12 21:55:20 -0700

    Add commented VCR configuration, to update later

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# VCR.configure do |config|
+#   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+#   config.hook_into :webmock
+#   # config.filter_sensitive_data('<HIDDEN_KEY>') { Rails.application.credentials.tmdb_movies[:key] }
+#   config.configure_rspec_metadata!
+#   config.default_cassette_options = { re_record_interval: 20.days, record: :new_episodes }
+# end
