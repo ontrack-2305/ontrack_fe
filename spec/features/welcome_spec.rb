06ac08277ebfc632087e8b97ff603f88b580fe65
Dani Wilson <danirae.powell@gmail.com>
2023-09-12 19:11:26 -0600

    add tests

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe "The Welcome Page" do
     expect(page).to have_button("Login With Google")
     expect("OnTrack").to appear_before("Login With Google")
   end
+
+  it "allows me to register for the app with Google" do
+    visit root_path
+
+    click_button "Login With Google"
+
+    expect(current_path).to eq(root_path)
+  end
 end
