c47ddd7f06e8103eeed185910e6298e77130d766
Artemy Gibson <artemygibson@gmail.com>
2023-09-12 19:21:46 -0700

    Create tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController 
+  # before_action :find_user
+  
+  def index 
+    
+  end
+
+  def new 
+
+  end
+
+  private 
+  
+  # def find_user
+    #begin 
+      # @user = User.find(session[:user_id])
+    # rescue
+      # redirect_to root_path
+      # flash[:notice] = "Please log in"
+    # end
+  # end
+end
