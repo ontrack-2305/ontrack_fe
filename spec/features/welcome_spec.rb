4b221bb10f87ebbe43f0727af5d4540f071a760f
Dani Wilson <danirae.powell@gmail.com>
2023-09-13 14:28:14 -0600

    add stub to before each

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
+require "omniauth_module"
 
 RSpec.describe "The Welcome Page" do
+  include OmniauthModule
+  before(:each) do
+    stub_omniauth
+  end
   it "displays the logo/app name and Google login button" do
     visit root_path
 
@@ -11,7 +16,7 @@ RSpec.describe "The Welcome Page" do
 
   it "allows me to register for the app with Google" do
     visit root_path
-    # save_and_open_page
+
     click_button "Login With Google"
 
     expect(current_path).to eq(root_path)
