38cc55835bf59070e2d153d7cc2e83b61bb0c962
Dani Wilson <danirae.powell@gmail.com>
2023-09-12 19:12:05 -0600

    add json and shoulda matchers stuff

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'simplecov'
 SimpleCov.start
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+require 'json'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -62,4 +63,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
