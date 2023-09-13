d6c8cf51198c27052fbd3a0da35e816e493b1a4b
Artemy Gibson <artemygibson@gmail.com>
2023-09-12 21:51:34 -0700

    Add task_params, time_needed, and starter create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,34 @@
 class TasksController < ApplicationController 
   # before_action :find_user
+  # before_action :convert_time_needed, only: [:create]
   
   def index 
-    
+
   end
 
   def new 
 
   end
 
+  def create
+    response = DatabaseService.new.post(task_params) #pass in user_id
+    # Refactor to facade later
+    redirect_to new_task_path if params[:create_another]
+    redirect_to dashboard_path if params[:commit]
+    flash[:notice] = JSON.parse(response.body)["message"]
+  end
+
   private 
+
+  def task_params
+    hash = params.permit(:name, :type, :mandatory, :event_date, :frequency, :description, :estimated_time, :prerequisite).to_h
+    hash[:time_needed] = time_needed
+    hash
+  end
+
+  def time_needed
+    params[:hours].to_i * 60 + params[:minutes].to_i
+  end
   
   # def find_user
     #begin 
