baacb8bc9a4fbefaec937cf542f8de5557be829b
Artemy Gibson <artemygibson@gmail.com>
2023-09-13 13:37:20 -0700

    Add testing for TasksFacade post and index points

--- spec/facades/tasks_facade_spec.rb
+++ spec/facades/tasks_facade_spec.rb
@@ -1,7 +1,46 @@
 require "rails_helper"
 
 RSpec.describe TasksFacade do
-  xit "can post a task" do
+  it "can post a task" do
+    json_response = {message: "'Water Plants' added!"}.to_json
+    stub_request(:post, "http://our_render_url.com/api/v1/users/1/tasks?event_date=&frequency=Weekly&mandatory=1&name=Water%20Plants&notes=Remember%20plants%20in%20bedroom,%20living%20room,%20and%20balcony&time_needed=20&type=Chore").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Length'=>'0',
+          'User-Agent'=>'Faraday v2.7.11'
+           }).
+         to_return(status: 200, body: json_response)
+    
+    facade = TasksFacade.new 
+    response = facade.post({"name"=>"Water Plants",
+      "type"=>"Chore",
+      "mandatory"=>"1",
+      "event_date"=>"",
+      "frequency"=>"Weekly",
+      "notes"=>"Remember plants in bedroom, living room, and balcony",
+      "time_needed"=>20}, "1")
 
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed_response[:message]).to eq("'Water Plants' added!")
+  end
+
+  it "can fetch all tasks" do
+    json_response = File.read("spec/fixtures/mock_tasks.json")
+    stub_request(:get, "http://our_render_url.com/api/v1/users/2/tasks").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v2.7.11'
+      }).
+    to_return(status: 200, body: json_response)
+
+
+    facade = TasksFacade.new
+    tasks = facade.get_tasks("2")
+    expect(tasks).to be_an(Array)
+    expect(tasks).to all be_a(Task)
   end
 end
