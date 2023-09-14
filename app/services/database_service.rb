d2e4e0d34e8c3348a4fdedd83a6866971153c52c
Artemy Gibson <artemygibson@gmail.com>
2023-09-14 09:28:42 -0700

    Add patch method

--- app/services/database_service.rb
+++ app/services/database_service.rb
@@ -7,6 +7,14 @@ class DatabaseService
     end
   end
 
+  def patch(attributes_hash, user_id)
+    connection.patch("api/v1/users/#{user_id}/tasks/#{attributes_hash["id"]}") do |faraday|
+      attributes_hash.each do |key, value|
+        faraday.params[key] = value if key != "id"
+      end
+    end
+  end
+
   def get_task(task_id, user_id)
     connection.get("api/v1/users/#{user_id}/tasks/#{task_id}")
   end
