a65536e78b6ed3bdde407fa7c1241c56d47cf75f
Dani Wilson <danirae.powell@gmail.com>
2023-09-14 13:55:20 -0600

    edit validations to reflect updated table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email, :token, :google_id
+  validates_presence_of :first_name, :last_name, :email, :token, :refresh_token, :google_id
 
-  def self.from_omniauth(response)
-    User.find_or_create_by(google_id: response[:uid]) do |u|
-      u.name = response[:info][:name]
-      u.email = response[:info][:email]
-      u.token = response[:credentials][:token]
-    end
+  def self.update_or_create(auth)
+    user = User.find_by(google_id: auth[:uid]) || User.new
+    user.attributes = {
+      google_id: auth[:uid],
+      email: auth[:info][:email],
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+      token: auth[:credentials][:token],
+      refresh_token: auth[:credentials][:refresh_token]
+    }
+    user.save!
+    user
   end
 end
