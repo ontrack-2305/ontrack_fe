cde43cff3d4f676c2df0a2d362b91ea914dcd0de
Dani Wilson <danirae.powell@gmail.com>
2023-09-13 18:03:46 -0600

    edit create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   
   def create
     user_info = request.env['omniauth.auth']
-    user = User.find_or_create_by(google_id: user_info['uid'])
+    # require 'pry'; binding.pry
+    user = User.find_or_create_by(google_id: user_info[:uid])
     user.name = user_info[:info][:name]
     user.email = user_info[:info][:email]
     user.token = user_info[:credentials][:token]
