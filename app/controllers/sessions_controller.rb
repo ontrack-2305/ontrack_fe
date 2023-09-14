a44509e0b492cf2b073abfafde7332eac67fa10b
Dani Wilson <danirae.powell@gmail.com>
2023-09-14 13:54:40 -0600

    modify create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < ApplicationController
-#   def new
-#   end
-
   def create
-    user = User.from_omniauth(request.env['omniauth.auth'])
+    user = User.update_or_create(request.env['omniauth.auth'])
     if user.valid?
       session[:user_id] = user.id
       redirect_to dashboard_path
@@ -11,21 +8,4 @@ class SessionsController < ApplicationController
       redirect_to "/"
     end
   end
-  
-  # def create
-  #   user_info = request.env['omniauth.auth']
-  #   user = User.find_or_create_by(google_id: user_info[:uid])
-  #   require 'pry'; binding.pry
-  #   user.name = user_info[:info][:name]
-  #   user.email = user_info[:info][:email]
-  #   user.token = user_info[:credentials][:token]
-
-  #   if user.save
-  #     session[:user_id] = user.id 
-  #     redirect_to dashboard_path(user.id)
-  #   else
-  #     flash[:error] = "Invalid credentials"
-  #     redirect_to root_path
-  #   end
-  # end
 end
