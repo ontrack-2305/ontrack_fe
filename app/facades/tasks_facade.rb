caa71e11630591f3489b5ebbb921ab55a2a67154
Artemy Gibson <artemygibson@gmail.com>
2023-09-14 09:27:58 -0700

    Add patch, add user id to relevant methods

--- app/facades/tasks_facade.rb
+++ app/facades/tasks_facade.rb
@@ -1,11 +1,29 @@
 class TasksFacade 
+  def initialize
+    @tasks_by_id = {}
+  end
 
   def post(attributes_hash, user_id)
     service.post(attributes_hash, user_id)
   end
 
+  def patch(attributes_hash, user_id)
+    @tasks_by_id[attributes_hash["id"]] = nil
+    service.patch(attributes_hash, user_id)
+  end
+
   def get_tasks(user_id)
-    @_tasks ||= fetch_tasks(user_id)
+    @task_list ||= fetch_tasks(user_id)
+  end
+
+  def get_task(task_id, user_id)
+    @tasks_by_id[task_id] ||= fetch_task(task_id, user_id)
+  end
+
+  def fetch_task(task_id, user_id)
+    response = service.get_task(task_id, user_id)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    Task.new(parsed[:data][0])
   end
 
   def fetch_tasks(user_id)
