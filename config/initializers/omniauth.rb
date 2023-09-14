8d14e669992ac1764cbef1e2d75116906476a5c5
Dani Wilson <danirae.powell@gmail.com>
2023-09-13 18:04:21 -0600

    add scope to omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-    {   
-      name: 'google',
-      scope: ['email, profile'],
-      prompt: 'select_account',
-      image_aspect_ratio: 'square',
-      image_size: 50
-    }
+  {
+    scope: ['email, profile'],
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 50,
+    provider_ignores_state: true
+  }
 end
