848c1490411bd3c7bec5aac6e60512a409d043a4
Dani Wilson <danirae.powell@gmail.com>
2023-09-13 19:21:27 -0600

    add from omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :token, :google_id
+
+  def self.from_omniauth(response)
+    User.find_or_create_by(google_id: response[:uid]) do |u|
+      u.name = response[:info][:name]
+      u.email = response[:info][:email]
+      u.token = response[:credentials][:token]
+    end
+  end
 end
