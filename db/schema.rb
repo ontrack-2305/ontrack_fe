41d3fc1ca9831b066776644ff617257783325bee
Dani Wilson <danirae.powell@gmail.com>
2023-09-14 11:49:33 -0600

    edit the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_233356) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_174737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "token"
     t.string "google_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "refresh_token"
   end
 
 end
