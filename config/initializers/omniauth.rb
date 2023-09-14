c35854de9c6f862cf0171d11133b47bfc87e5e90
Dani Wilson <danirae.powell@gmail.com>
2023-09-14 13:55:58 -0600

    modify omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-    {
-      scope: ['email, profile'],
-      prompt: 'select_account',
-      image_aspect_ratio: 'square',
-      image_size: 50,
-      provider_ignores_state: true
-    }
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+    :name => "google",
+    :scope => ['contacts','plus.login','plus.me','email','profile'],
+    :prompt => "select_account",
+    :image_aspect_ratio => "square",
+    :image_size => 50,
+    :access_type => 'offline'
+  }
 end
