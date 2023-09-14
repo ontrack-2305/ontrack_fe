d6d0de9895e132d1b0dc803a88789ca05dc9b9c7
Artemy Gibson <artemygibson@gmail.com>
2023-09-14 09:03:36 -0700

    Add show and update actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,12 +5,16 @@ class TasksController < ApplicationController
 
   end
 
+  def show
+    @task = facade.get_task(params[:id], session[:user_id])
+  end
+
   def new 
 
   end
 
   def create
-    response = TasksFacade.new.post(task_params, session[:user_id])
+    response = facade.post(task_params, session[:user_id])
     if response.status == 200
       redirect_to new_task_path if params[:create_another]
       redirect_to dashboard_path if params[:commit]
@@ -18,20 +22,30 @@ class TasksController < ApplicationController
     elsif response.status == 400
       redirect_to new_task_path 
       flash[:notice] = JSON.parse(response.body)["errors"][0]["detail"]
-      # incorporate ActionCable to stay on page instead of refresh?
     end
+  end
+
+  def update 
+    response = facade.patch(task_params, session[:user_id])
+    redirect_to task_path(params[:id])
+    flash[:notice] = JSON.parse(response.body)["message"]
+    flash[:notice] = JSON.parse(response.body)["errors"][0]["detail"] if response.status == 400
   end
 
   private 
 
   def task_params
-    hash = params.permit(:name, :type, :mandatory, :event_date, :frequency, :notes, :estimated_time).to_h
+    hash = params.permit(:name, :category, :mandatory, :event_date, :frequency, :notes, :estimated_time, :id).to_h
     hash[:time_needed] = time_needed
     hash
   end
 
   def time_needed
     params[:hours].to_i * 60 + params[:minutes].to_i
+  end
+
+  def facade 
+    @_facade ||= TasksFacade.new
   end
   
   # def find_user
