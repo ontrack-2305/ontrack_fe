1db0497f1dc61298c6847212c010fd8ae95edddc
Dani Wilson <danirae.powell@gmail.com>
2023-09-15 08:35:38 -0600

    edit file to use rails credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
-    :name => "google",
+  provider :google_oauth2, Rails.application.credentials[:GOOGLE_CLIENT_ID], Rails.application.credentials[:GOOGLE_CLIENT_SECRET], {
+    :name => "google_oauth2",
     :scope => ['contacts','plus.login','plus.me','email','profile'],
     :prompt => "select_account",
     :image_aspect_ratio => "square",
