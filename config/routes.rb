2618ecd5fefdb0b5a88a74d7104adad919359be4
Dani Wilson <danirae.powell@gmail.com>
2023-09-15 08:35:57 -0600

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get "/dashboard", to: "users#show"
   resources :tasks, only: [:new, :index, :create, :show, :update, :destroy]
 
-  get 'auth/:provider/callback',  to: 'sessions#create'
-  # get 'auth/failure', to: redirect('/')
+  get 'auth/:provider', to: 'sessions#create', as: "google_login"
+  get 'auth/google_oauth2/callback',  to: 'sessions#create'
 
   resources :tasks, only: [:new, :index, :create]
 end
