5971a1418cc645c77c8da01c479e6fc3f0c523f7
Dani Wilson <danirae.powell@gmail.com>
2023-09-15 17:40:30 -0600

    add a destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,10 @@ class SessionsController < ApplicationController
       redirect_to dashboard_path
     end
   end
+
+  def destroy
+    session.delete(:user_id)
+    @user = nil
+    redirect_to root_path
+  end
 end
