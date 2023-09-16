ba73dd064ad14f6c38888e608f0524a1059d771c
Artemy Gibson <artemygibson@gmail.com>
2023-09-15 20:06:07 -0700

    Add comment about user variable being possibly redundant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(params[:google_id])
+    # does this need to be defined at all if there's already a @_current_user?
   end
 end
