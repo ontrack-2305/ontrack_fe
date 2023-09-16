e4c51a345bada7925c8c0ce97b552e2430d742b8
Artemy Gibson <artemygibson@gmail.com>
2023-09-16 08:13:59 -0700

    Add before_action to check user is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :validate_session
+  
   def show
     @user = User.find_by(params[:google_id])
     # does this need to be defined at all if there's already a @_current_user?
