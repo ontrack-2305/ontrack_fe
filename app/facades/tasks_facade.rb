8aeef0c0b8485d87a90ea65d413ed0dfc213b803
Artemy Gibson <artemygibson@gmail.com>
2023-09-16 12:57:30 -0700

    Remove memoization

--- app/facades/tasks_facade.rb
+++ app/facades/tasks_facade.rb
@@ -1,23 +1,24 @@
 class TasksFacade 
-  def initialize
-    @tasks_by_id = {}
-  end
-
   def post(attributes_hash, user_id)
     service.post(attributes_hash, user_id)
   end
 
   def patch(attributes_hash, user_id)
-    @tasks_by_id[attributes_hash["id"]] = nil
     service.patch(attributes_hash, user_id)
   end
 
   def get_tasks(user_id)
-    @task_list ||= fetch_tasks(user_id)
+    response = service.get_tasks(user_id)
+    task_hashes = JSON.parse(response.body, symbolize_names: true)[:data]
+    task_hashes.map do |task_hash|
+      Task.new(task_hash[:attributes], task_hash[:id])
+    end
   end
 
   def get_task(task_id, user_id)
-    @tasks_by_id[task_id] ||= fetch_task(task_id, user_id)
+    response = service.get_task(task_id, user_id)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    Task.new(parsed[:data][:attributes], parsed[:data][:id])
   end
 
   def delete(task_id, user_id)
@@ -33,19 +34,7 @@ class TasksFacade
     end
   end
 
-  def fetch_task(task_id, user_id)
-    response = service.get_task(task_id, user_id)
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    Task.new(parsed[:data][0][:attributes])
-  end
-
-  def fetch_tasks(user_id)
-    response = service.get_tasks(user_id)
-    task_hashes = JSON.parse(response.body, symbolize_names: true)[:data]
-    task_hashes.map do |task_hash|
-      Task.new(task_hash[:attributes])
-    end
-  end
+  private
 
   def service
     @_service ||= DatabaseService.new
