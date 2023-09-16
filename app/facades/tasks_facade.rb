471e91ad4a7fb55470b2597bd35a5a58c26cfbb6
Artemy Gibson <artemygibson@gmail.com>
2023-09-16 08:13:38 -0700

    Add method get_ai_breakdown

--- app/facades/tasks_facade.rb
+++ app/facades/tasks_facade.rb
@@ -24,17 +24,26 @@ class TasksFacade
     service.destroy(task_id, user_id)
   end
 
+  def get_ai_breakdown(task_name)
+    response = service.get_ai_breakdown(task_name)
+    if response.status == 200
+      JSON.parse(response.body, symbolize_names: true)[:response][0][:text]
+    else
+      JSON.parse(response.body, symbolize_names: true)[:errors][0][:title]
+    end
+  end
+
   def fetch_task(task_id, user_id)
     response = service.get_task(task_id, user_id)
     parsed = JSON.parse(response.body, symbolize_names: true)
-    Task.new(parsed[:data][0])
+    Task.new(parsed[:data][0][:attributes])
   end
 
   def fetch_tasks(user_id)
     response = service.get_tasks(user_id)
     task_hashes = JSON.parse(response.body, symbolize_names: true)[:data]
     task_hashes.map do |task_hash|
-      Task.new(task_hash)
+      Task.new(task_hash[:attributes])
     end
   end
 
