9f2e3c48bb36502e07307ad3a5978bca7a6955f9
Artemy Gibson <artemygibson@gmail.com>
2023-09-16 13:04:36 -0700

    Add tests for post, patch, and destroy service calls

--- spec/services/database_service_spec.rb
+++ spec/services/database_service_spec.rb
@@ -1,40 +1,67 @@
 require "rails_helper"
 
-RSpec.describe DatabaseService do
+RSpec.describe DatabaseService, :vcr do
+  before(:each) do
+    @service = DatabaseService.new
+    @attributes_hash = {:name=>"Water Plants", :category=>"chore", :mandatory=>"1", :event_date=>"", :frequency=>"weekly", :notes=>"Remember plants in bedroom, living room, and balcony", :time_needed=>20}
+    # @attributes_hash_with_id = {:id => "26", :name=>"Water Plants", :category=>"chore", :mandatory=>"1", :event_date=>"", :frequency=>"weekly", :notes=>"Remember plants in bedroom, living room, and balcony", :time_needed=>20}
+  end
+
   it "connects to database" do
-    service = DatabaseService.new
-    response = service.connection 
+    response = @service.connection 
     expect(response).to be_a(Faraday::Connection)
   end
 
-  xit "retrieves a task" do
-    service = DatabaseService.new
-    # response = service.get_task("1")
-    response = stub_request(:get, "http://our_render_url.com/api/v1/users/1/tasks/1").
-    with(
-      headers: {
-      'Accept'=>'*/*',
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'User-Agent'=>'Faraday v2.7.11'
-      }).
-    to_return(status: 200, body: "", headers: {})
+  xit "posts a task" do
+    response = @service.post(@attributes_hash, "24")
+    message = JSON.parse(response.body, symbolize_names: true)[:message] ##pending update from BE
+    expect(message).to eq("'Water Plants' added!")
+    fetched = @service.get_tasks("24")
+    data = JSON.parse(fetched.body, symbolize_names: true)[:data][-1]
+    expect(data[:attributes][:name]).to eq("Water Plants")
+  end
+
+  xit "patches a task" do
+    @service.post(@attributes_hash, "24")
+    response1 = @service.get_tasks("24")
+    task_id = JSON.parse(response1.body, symbolize_names: true)[:data][-1][:id]
+
+    response2 = @service.patch({frequency: "daily", id: task_id}, "24")
+    message = JSON.parse(response2.body, symbolize_names: true)[:message]
+    expect(message).to eq("Changes saved") ##pending update from BE
+    
+    fetched = @service.get_task(task_id, "24")
+    data = JSON.parse(fetched.body, symbolize_names: true)[:data]
+
+    expect(data[:attributes][:name]).to eq("Water Plants")
+    expect(data[:attributes][:frequency]).to eq("daily")
+  end
+
+  xit "destroys a task" do
+    @service.post(@attributes_hash, "24")
+    response1 = @service.get_tasks("24")
+    task_id = JSON.parse(response1.body, symbolize_names: true)[:data][-1][:id]
+
+    response2 = @service.destroy(task_id, "24")
+    message = JSON.parse(response2.body, symbolize_names: true)[:message]
+    expect(message).to eq("Changes saved") ##pending update from BE
+    
+    fetched = @service.get_task(task_id, "24")
+    data = JSON.parse(fetched.body, symbolize_names: true)[:data]
+
+    expect(data[:attributes][:name]).to eq("Water Plants")
+    expect(data[:attributes][:frequency]).to eq("daily")
+  end
+
+  xit "gets one task" do
 
   end
 
-  xit "accepts a request to post a new task" do
-    service = DatabaseService.new
-    response = service.post({"name"=>"Water Plants",
-      "category"=>"Chore",
-      "mandatory"=>"1",
-      "event_date"=>"",
-      "frequency"=>"Weekly",
-      "notes"=>"Remember plants in bedroom, living room, and balcony",
-      "time_needed"=>20}, "1") 
+  xit "gets all tasks for a user" do
 
-      #pending OAuth working
   end
 
-  xit "updates a task" do
+  xit "gets an ai breakdown" do
 
   end
 end
