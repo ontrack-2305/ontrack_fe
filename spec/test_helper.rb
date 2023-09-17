16141b4a01e6c2e29856186ef45b39df8a2ca871
Artemy Gibson <artemygibson@gmail.com>
2023-09-16 19:14:15 -0700

    Add stub_user helper method

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,4 +1,10 @@
-
+def stub_user
+  @user = User.create({id: "123", first_name: "John"})
+  allow(User).to receive(:update_or_create).and_return(user)
+  allow(user).to receive(:errors).and_return(nil)
+  allow(User).to receive(:find_by).and_return(user)
+  allow(User).to receive(:find).and_return(user)
+end
 
 def generate_tasks_for(user)
   @facade.post({:name=>"Water Plants", :category=>"chore", :mandatory=>"1", :event_date=>"", :frequency=>"weekly", :notes=>"Remember plants in bedroom, living room, and balcony", :time_needed=>20}, user.id)
