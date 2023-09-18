a61156abdeded9f94004cdc00a60c02466e51b13
Dani Wilson <danirae.powell@gmail.com>
2023-09-18 15:16:29 -0600

    begin testing for dashboard

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -26,25 +26,40 @@ RSpec.describe "the user dashboard page", :vcr do
       "notes"=>"he shed",
       "time_needed"=>5}, @user.id)
 
+      @facade.post({"name"=>"Shower",
+      "category"=>"chore",
+      "mandatory"=>"1",
+      "event_date"=>"",
+      "frequency"=>"daily",
+      "notes"=>"",
+      "time_needed"=>15}, @user.id)
+
       @mandatory_task = @facade.get_tasks(@user.id).first
-      @non_mandatory_task = @facade.get_tasks(@user.id).last
+      @non_mandatory_task = @facade.get_tasks(@user.id).second
+      @another_mandatory = @facade.get_tasks(@user.id).last
     end
 
     after(:each) do
-      @facade.delete(@task.id, @user.id)
+      @facade.delete(@mandatory_task.id, @user.id)
+      @facade.delete(@non_mandatory_task.id, @user.id)
+      @facade.delete(@another_mandatory.id, @user.id)
     end
 
-    it "displays a flash welcome message with the user first name" do
+    it "displays a welcome message with the user first name" do #edit to be a non-persisting flash message only
       expect(page).to have_content("Welcome, John!")
     end
 
-    it "prompts me to input my mood" do
+    it "displays a mood button for 'meh', 'good', and 'bad' days" do
       visit dashboard_path
 
-      expect(page).to have_button("Meh")
-      expect(page).to have_button("Good")
-      expect(page).to have_button("Bad")
+      page.has_css?("good_button")
+      page.has_css?("meh_button")
+      page.has_css?("bad_button")
     end
 
+    it "when I click on a button, that button stays highlighted" do
+      visit dashboard_path
 
+      click_button("happy face button image")
+    end
   end
