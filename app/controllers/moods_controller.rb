b2c6c6b1f6c1acd4025678cb3673c622a7c75d5f
Nick Sacco <nicksacco10@gmail.com>
2023-09-18 16:33:07 -0700

    feat: updated users controller show action
    CO-AUTHORED-BY <danirae.powell@gmail.com>

--- app/controllers/moods_controller.rb
+++
@@ -1,7 +0,0 @@
-class MoodsController < ApplicationController
-  def create
-    # response = facade.post(mood_params, session[:user_id])
-    # if response.status == 201
-    # require 'pry'; binding.pry
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_action :validate_session
   
   def show
+    mood = params[:mood]
     @user = User.find_by(params[:google_id])
+  
+    # @tasks = TasksFacade.new.tasks_by_mood(@user.id, mood)
     # does this need to be defined at all if there's already a @_current_user?
   end
 end
