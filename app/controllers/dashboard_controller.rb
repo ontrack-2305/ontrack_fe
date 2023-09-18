2c57c73b6be27af609db34ae4d3b151941012f30
Dani Wilson <danirae.powell@gmail.com>
2023-09-18 15:15:41 -0600

    create and then delete dashboard controller

--- app/controllers/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class DashboardController < ApplicationController
-  # def index
-  #   unless session[:welcome_message_displayed]
-  #     flash[:notice] = "Welcome, #{@_current_user.name}!"
-  #     session[:welcome_message_displayed]
-  #   end
-  # end
-end
