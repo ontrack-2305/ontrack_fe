cc4a17681119695b01064ed2a63ff2d720bddbfd
Anna Wiley <awiley33@gmail.com>
2023-09-18 10:47:13 -0500

    feat: Add wip styling navbar, dashboard page

--- spec/facades/tasks_facade_spec.rb
+++ spec/facades/tasks_facade_spec.rb
@@ -1,20 +1,36 @@
 require "rails_helper"
 
 RSpec.describe TasksFacade, :vcr do
-  xit "can post a task" do    
+  before(:each) do
+    @user_id = "20"
+  end
+
+  after(:each) do
+    facade = TasksFacade.new
+    begin
+      tasks = facade.get_tasks(@user_id)
+    rescue
+      tasks = []
+    end
+    tasks.each do |task|
+      facade.delete(task.id, @user_id)
+    end
+  end
+
+  it "can post a task" do    
     facade = TasksFacade.new 
-    count = facade.get_tasks("1").count
+    expect(facade.get_tasks(@user_id).count).to eq(0)
     response = facade.post({"name"=>"Water Plants",
       "category"=>"chore",
       "mandatory"=>"1",
       "event_date"=>"",
       "frequency"=>"weekly",
       "notes"=>"Remember plants in bedroom, living room, and balcony",
-      "time_needed"=>20}, "1")
+      "time_needed"=>20}, @user_id)
 
-    expect(facade.get_tasks("1").count).to eq(count + 1)
+    expect(facade.get_tasks(@user_id).count).to eq(1)
     parsed_response = JSON.parse(response.body, symbolize_names: true)
-    expect(parsed_response[:message]).to eq("'Water Plants' added!") ## pending backend update
+    expect(parsed_response[:message]).to eq("'Water Plants' added!")
   end
 
   it "can fetch all tasks" do
@@ -25,35 +41,38 @@ RSpec.describe TasksFacade, :vcr do
       "event_date"=>"",
       "frequency"=>"weekly",
       "notes"=>"Remember plants in bedroom, living room, and balcony",
-      "time_needed"=>20}, "1")
+      "time_needed"=>20}, @user_id)
     facade.post({"name"=>"Prune Plants",
         "category"=>"chore",
         "mandatory"=>"1",
         "event_date"=>"",
         "frequency"=>"weekly",
         "notes"=>"Remember plants in bedroom, living room, and balcony",
-        "time_needed"=>20}, "1")
+        "time_needed"=>20}, @user_id)
 
-    tasks = facade.get_tasks("1")
+    tasks = facade.get_tasks(@user_id)
     expect(tasks).to be_an(Array)
     expect(tasks).to all be_a(Task)
-    expect(tasks.count >= 2).to eq(true)
+    expect(tasks.count).to eq(2)
   end
 
-  xit "can update a task" do
+  it "can update a task" do
     facade = TasksFacade.new
-    response = facade.patch({
-      "id"=>"1",
+    facade.post({
       "name"=>"Water Plants",
-      "category"=>"Chore",
+      "category"=>"chore",
       "mandatory"=>"1",
       "event_date"=>"",
-      "frequency"=>"Weekly",
+      "frequency"=>"weekly",
       "notes"=>"Remember plants in bedroom, living room, and balcony",
-      "time_needed"=>20}, "1")
-    
+      "time_needed"=>20}, @user_id)
+
+    task = facade.get_tasks(@user_id).last
+
+    response = facade.patch({notes: "Different notes", id: task.id}, @user_id)
+
     parsed = JSON.parse(response.body, symbolize_names: true)
-    expect(parsed[:message]).to eq("Changes saved!")  ##pending backend update
+    expect(parsed[:message]).to eq("Changes saved!")
   end
 
   it "can get one task" do
@@ -64,15 +83,16 @@ RSpec.describe TasksFacade, :vcr do
       "event_date"=>"",
       "frequency"=>"weekly",
       "notes"=>"Remember plants in bedroom, living room, and balcony",
-      "time_needed"=>20}, "1")
-    tasks = facade.get_tasks("1")
+      "time_needed"=>20}, @user_id)
+    tasks = facade.get_tasks(@user_id)
 
-    task = facade.get_task(tasks.last.id, "1")
+    task = facade.get_task(tasks.last.id, @user_id)
     expect(task).to be_a(Task)
     expect(task.name).to eq("Repot Plants")
   end
 
- xit "can delete a task" do
+  it "can delete a task" do
+    pending "backend removes 204 code from delete response"
     facade = TasksFacade.new
     facade.post({"name"=>"Water Plants",
       "category"=>"chore",
@@ -80,15 +100,15 @@ RSpec.describe TasksFacade, :vcr do
       "event_date"=>"",
       "frequency"=>"weekly",
       "notes"=>"Remember plants in bedroom, living room, and balcony",
-      "time_needed"=>20}, "1")
-    tasks = facade.get_tasks("1")
-    task = facade.get_task(tasks.first.id, "1")
-    task_count = tasks.count
-    response = facade.delete(task.id, "1")
-    expect(facade.get_tasks("1").count).to eq(task_count - 1)
+      "time_needed"=>20}, @user_id)
+    tasks = facade.get_tasks(@user_id)
+    task = facade.get_task(tasks.first.id, @user_id)
+    expect(facade.get_tasks(@user_id).count).to eq(1)
+    response = facade.delete(task.id, @user_id)
+    expect(facade.get_tasks(@user_id).count).to eq(0)
 
-    parsed = JSON.parse(response.body, symbolize_names: true) ##pending backend update
-    expect(parsed[:message]).to eq("'Water Plants' deleted.") # pending backend update
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed[:message]).to eq("'Water Plants' deleted.")
   end
 
   it "can fetch an AI breakdown for a task name" do
