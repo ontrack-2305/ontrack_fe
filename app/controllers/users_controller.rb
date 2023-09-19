06fba33af711c61b0044d7d3270623ed391bf438
Dani Wilson <danirae.powell@gmail.com>
2023-09-19 12:56:40 -0600

    refactor @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   
   def show
     mood = params[:mood]
-    @user = User.find_by(params[:google_id])
+    @user = current_user
     @task = TasksFacade.new.task_by_mood(@user.id, mood)
-    # does this need to be defined at all if there's already a @_current_user?
   end
 end
