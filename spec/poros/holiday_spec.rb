130cd2aa0ad012dedea0febc3f523ff048201539
Nick Sacco <nicksacco10@gmail.com>
2023-09-19 15:54:13 -0700

    test: holiday poro tests

---
+++ spec/poros/holiday_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Holiday do
+  it 'exists and has attributes', :vcr do
+    params = {
+      "date": "2023-10-09",
+      "name": "Columbus Day"
+    }
+
+    holiday = Holiday.new(params)
+
+    expect(holiday).to be_a(Holiday)
+    expect(holiday.name).to eq("Columbus Day")
+    expect(holiday.date).to eq("2023-10-09")
+  end
+end
