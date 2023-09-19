2095825bdec4b366a2acfa25c7e9a2221570dea4
Parker Boeing <parker.boeing@gmail.com>
2023-09-18 18:10:18 -0600

    wip: Add calendar to omniauth initializers scope

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @user = current_user
-    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
     service = DatabaseService.new
     response = service.authenticate(@user)
     # does this need to be defined at all if there's already a @_current_user?
