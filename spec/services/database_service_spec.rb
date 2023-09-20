213c1691e59c7adee6c88697ffc4078300f99dee
Nick Sacco <nicksacco10@gmail.com>
2023-09-19 19:09:40 -0700

    refactor: move holiday service tests over to database service

--- spec/services/database_service_spec.rb
+++ spec/services/database_service_spec.rb
@@ -162,4 +162,26 @@ RSpec.describe DatabaseService, :vcr do
     expect(parsed[:data][1][:attributes][:category]).to eq("chore")
     expect(parsed[:data][2][:attributes][:category]).to eq("hobby")
   end
+
+  describe "consumes the Holiday API" do
+    it "returns a list of holidays", :vcr do
+      holidays = DatabaseService.new.get_holidays
+
+      expect(holidays).to be_a(Hash)
+      expect(holidays).to have_key(:data)
+      expect(holidays[:data]).to be_an(Array)
+
+      holidays[:data].each do |holiday|
+        expect(holiday).to have_key(:type)
+        expect(holiday[:type]).to be_a(String)
+        expect(holiday).to have_key(:attributes)
+        expect(holiday[:attributes]).to be_a(Hash)
+
+        expect(holiday[:attributes]).to have_key(:name)
+        expect(holiday[:attributes][:name]).to be_a(String)
+        expect(holiday[:attributes]).to have_key(:date)
+        expect(holiday[:attributes][:date]).to be_a(String)
+      end
+    end
+  end
 end
