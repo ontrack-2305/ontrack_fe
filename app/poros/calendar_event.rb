3f8b023051ef7c4ecb71f593b3c9a6fc7988ca31
Nick Sacco <nicksacco10@gmail.com>
2023-09-19 20:27:00 -0700

    dem skeletons yo

---
+++ app/poros/calendar_event.rb
@@ -0,0 +1,9 @@
+class CalendarEvent
+  attr_reader :name,
+              :start_date,
+              :description
+
+  def initialize(attributes)
+    
+  end
+end
---
+++ spec/poros/calendar_event_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe CalendarEvent do
+  describe 'creating a calendar event' do
+    xit 'can create a calendar event' do
+      params = {
+      }
+    end
+  end
+end
