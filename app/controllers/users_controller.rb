1eeb6325cfe01aef1530ec40c6e721486fe902d1
Nick Sacco <nicksacco10@gmail.com>
2023-09-20 12:09:50 -0700

    facade wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
       # service = DatabaseService.new
       # response = service.get_calendar_events(current_user)
       # parsed_response = JSON.parse(response.body)
-      @calendar_events = CalendarEventFacade.new.calendar_events
-    end
+      # end
       mood = params[:mood]
       @user = current_user
       @task = TasksFacade.new.task_by_mood(@user.id, mood)
       @holidays = HolidayFacade.upcoming_holidays
+      @calendar_events = CalendarEventFacade.new.calendar_events(current_user)
+      # require 'pry'; binding.pry
   end
 end
--- app/facades/calendar_event_facade.rb
+++ app/facades/calendar_event_facade.rb
@@ -1,10 +1,11 @@
 class CalendarEventFacade
-  def calendar_events
+  def calendar_events(current_user)
     service = DatabaseService.new
     response = service.get_calendar_events(current_user)
     parsed_response = JSON.parse(response.body)
+    # require 'pry'; binding.pry
     parsed_response[:data].map do |event|
-      CalendarEvent.new(event)
+      truth = CalendarEvent.new(event)
     end
   end
 end
