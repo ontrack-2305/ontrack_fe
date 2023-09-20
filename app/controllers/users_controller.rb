051a589f876a0cb8e2a4b98e5355b1702312cb8b
Nick Sacco <nicksacco10@gmail.com>
2023-09-20 12:40:00 -0700

    feat: calendar events showing on page, yusss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,12 @@ class UsersController < ApplicationController
   before_action :validate_session
   
   def show
-    # if params[:calendar] == "true"
-      # service = DatabaseService.new
-      # response = service.get_calendar_events(current_user)
-      # parsed_response = JSON.parse(response.body)
-      # end
-      mood = params[:mood]
-      @user = current_user
-      @task = TasksFacade.new.task_by_mood(@user.id, mood)
-      @holidays = HolidayFacade.upcoming_holidays
+    mood = params[:mood]
+    @user = current_user
+    @task = TasksFacade.new.task_by_mood(@user.id, mood)
+    @holidays = HolidayFacade.upcoming_holidays
+    if params[:calendar] == "true"
       @calendar_events = CalendarEventFacade.new.calendar_events(current_user)
-      # require 'pry'; binding.pry
+    end
   end
 end
--- app/facades/calendar_event_facade.rb
+++ app/facades/calendar_event_facade.rb
@@ -3,9 +3,10 @@ class CalendarEventFacade
     service = DatabaseService.new
     response = service.get_calendar_events(current_user)
     parsed_response = JSON.parse(response.body)
-    # require 'pry'; binding.pry
-    parsed_response[:data].map do |event|
-      truth = CalendarEvent.new(event)
+    events_data = parsed_response['data']
+    calendar_events = events_data.map do |event_data|
+      event_attributes = event_data['attributes']
+      CalendarEvent.new(event_attributes)
     end
   end
 end
