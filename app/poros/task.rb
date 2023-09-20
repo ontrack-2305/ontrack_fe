e234ef2efd1849b3b343ef2a95381765fd9fc9d6
Artemy Gibson <artemygibson@gmail.com>
2023-09-19 21:41:19 -0700

    Add method to translate mandatory from a binary value to a boolean value

--- app/poros/task.rb
+++ app/poros/task.rb
@@ -2,7 +2,6 @@ class Task
   attr_reader :id,
               :name,
               :category,
-              :mandatory,
               :event_date,
               :time_needed,
               :frequency,
@@ -17,7 +16,7 @@ class Task
     @mandatory = data[:mandatory]
     @event_date = data[:event_date]
     @frequency = data[:frequency]
-    @time_needed = data[:time_needed]
+    @time_needed = data[:time_needed].to_i
     @notes = data[:notes]
     @user_id = data[:user_id]
   end
@@ -28,5 +27,11 @@ class Task
 
   def minutes 
     @time_needed % 60 unless @time_needed.nil?
+  end
+
+  def mandatory 
+    return true if @mandatory == "1"
+    return false if @mandatory == "0"
+    @mandatory
   end
 end
