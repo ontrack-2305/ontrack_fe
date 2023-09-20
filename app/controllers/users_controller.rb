650a4fd86979f866c6a93ad51548ee6ba388e6f9
Nick Sacco <nicksacco10@gmail.com>
2023-09-20 09:32:00 -0700

    feat: moved logic to dashboard controller
    CO-AUTHORED-BY <parker.boeing@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@ class UsersController < ApplicationController
   before_action :validate_session
   
   def show
-    mood = params[:mood]
-    @user = current_user
-    @task = TasksFacade.new.task_by_mood(@user.id, mood)
-    @holidays = HolidayFacade.upcoming_holidays
+    require 'pry'; binding.pry
+    if params[:calendar] == "true"
+      service = DatabaseService.new
+      response = service.get_calendar_events(current_user)
+      parsed_response = JSON.parse(response.body)
+    end
+      mood = params[:mood]
+      @user = current_user
+      @task = TasksFacade.new.task_by_mood(@user.id, mood)
+      @holidays = HolidayFacade.upcoming_holidays
   end
 end
