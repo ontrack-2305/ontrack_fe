f10e2792b64e5cef4398bc7e72d313fdf82cf9ab
Artemy Gibson <artemygibson@gmail.com>
2023-09-20 10:50:13 -0700

    Add skeleton for completed task test, add pending marker to skip task test

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -60,11 +60,11 @@ RSpec.describe "the user dashboard page", :vcr do
 
       tasks = @facade.get_tasks(@user.id)
 
-      @reading = tasks.first
-      @walk = tasks.second
-      @juggling = tasks.third
-      @vitamins = tasks.fourth
-      @crochet = tasks.fifth
+      @vitamins = tasks.first
+      @crochet = tasks.second
+      @reading = tasks.third
+      @walk = tasks.fourth
+      @juggling = tasks.fifth
       @dishes = tasks.last
     end
 
@@ -86,7 +86,7 @@ RSpec.describe "the user dashboard page", :vcr do
     it "only gets one task at a time with options" do
       click_button("happy face button image")
 
-      expect(page).to have_content(@walk.name)
+      expect(page).to have_content(@vitamins.name)
       expect(page).to have_button("skip")
       expect(page).to have_button("completed")
     end
@@ -94,22 +94,34 @@ RSpec.describe "the user dashboard page", :vcr do
     it "the mood and task persist if I leave the page" do
       click_button("happy face button image")
 
-      expect(page).to have_content(@walk.name)
+      expect(page).to have_content(@vitamins.name)
 
       visit new_task_path
 
       visit dashboard_path
       
-      expect(page).to have_content(@walk.name)
+      expect(page).to have_content(@vitamins.name)
       expect(page).to_not have_content("Please add a task!")
     end
 
     it "can display a warning flash message if skipping a mandatory task" do
+      pending "warning flash message implemented, skip functionality implemented"
       click_button("happy face button image")
 
-      expect(page).to have_content(@walk.name)
+      expect(page).to have_content(@vitamins.name)
       expect(page).to have_button("skip")
       click_button("skip")
       expect(page).to have_content("Are you sure you'd like to skip a mandatory task?")
+    end
+
+    xit "can move on to a new task" do
+      # extra: get_task from database and check that "completed" is empty
+
+      #visit page with a task on it
+      # click completed
+      # check that the page refreshes with a new task
+
+      # extra: get_task from database again and check that "completed" exists
+      # Can check that completed == stringified Date.today
     end
   end
