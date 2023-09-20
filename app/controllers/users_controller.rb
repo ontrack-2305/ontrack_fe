5d34a5d26b53f3ec439b6ec61c3a8b52f328331c
Artemy Gibson <artemygibson@gmail.com>
2023-09-20 10:46:18 -0700

    Rearrange show method so cookie is set first and @task is fetched last

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,9 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @mood = params[:mood]
-    @task = TasksFacade.new.task_by_mood(@user.id, @mood) 
     if @mood.present?
       cookies[:mood] = { value: @mood, expires: 1.year.from_now }
     end
-  end
-
-  def update
-    # skipped tasks will come here
-    # completed tasks will come here first, check if "once"
-    # if frequency == "once", route to destroy
-    # task.update(skipped = "true")
-  end
-
-  def destroy
-    # if frequency == "once"
-    # task.destroy
+    @task = TasksFacade.new.task_by_mood(@user.id, cookies[:mood]) 
   end
 end
