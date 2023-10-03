d495c8f294a7490128f2a50ce9f7899ee4e1b2a5
Nick Sacco <nicksacco10@gmail.com>
2023-10-03 14:30:08 -0700

    first sad path test implemented for password confirmation

--- spec/features/users/user_can_be_created_spec.rb
+++ spec/features/users/user_can_be_created_spec.rb
@@ -1,20 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration form' do
-  it 'creates a new user', :vcr do
-    visit root_path
+  describe 'Happy Path' do
+    it 'creates a new user', :vcr do
+      visit root_path
+      
+      click_on 'Create an Account'
+      
+      expect(current_path).to eq(new_user_path)
+      
+      fill_in :user_first_name, with: 'John'
+      fill_in :user_last_name, with: 'Doe'
+      fill_in :user_email, with: 'John@imjohn.com'
+      fill_in :user_password, with: 'password'
+      fill_in :user_password_confirmation, with: 'password'
+      click_button 'Create Account'
+      
+      expect(current_path).to eq(dashboard_path)
+    end
+  end
 
-    click_on 'Create an Account'
+  describe 'Sad Path' do
+    it 'does not create a new user if passwords do not match', :vcr do
+      visit root_path
+      
+      click_on 'Create an Account'
+      
+      expect(current_path).to eq(new_user_path)
+      
+      fill_in :user_first_name, with: 'John'
+      fill_in :user_last_name, with: 'Doe'
+      fill_in :user_email, with: 'jimo@doe.com'
+      fill_in :user_password, with: 'password'
+      fill_in :user_password_confirmation, with: 'PasSwoRd'
+      click_button 'Create Account'
 
-    expect(current_path).to eq(new_user_path)
-
-    fill_in :user_first_name, with: 'John'
-    fill_in :user_last_name, with: 'Doe'
-    fill_in :user_email, with: 'John@imjohn.com'
-    fill_in :user_password, with: 'password'
-    fill_in :user_password_confirmation, with: 'password'
-    click_button 'Create Account'
-
-    expect(current_path).to eq(dashboard_path)
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
   end
 end
