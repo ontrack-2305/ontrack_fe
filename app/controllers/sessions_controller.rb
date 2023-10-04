a3b292a7280d11adaa8cf576a93eab5003bcd275
Nick Sacco <nicksacco10@gmail.com>
2023-10-04 07:42:41 -0700

    feat: routes to controller action and login functionality to landing page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,23 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_google_auth(request.env['omniauth.auth'])
-    if user.errors.present?
-      flash[:alert] = user.errors.full_messages.join(', ')
-      redirect_to root_path
+    if params[:email] && params[:password]
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:success] = "Welcome, #{user.email}"
+        redirect_to dashboard_path
+      else
+        flash[:alert] = user.errors.full_messages.join(', ')
+      end
     else
-      session[:user_id] = user.id
-      redirect_to dashboard_path
+      user = User.from_google_auth(request.env['omniauth.auth'])
+      if user.errors.present?
+        flash[:alert] = user.errors.full_messages.join(', ')
+        redirect_to root_path
+      else
+        session[:user_id] = user.id
+        redirect_to dashboard_path
+      end
     end
   end
 
